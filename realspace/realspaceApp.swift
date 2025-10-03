880a0dd89e7ca00c2c62467cbe531022945cbf6b
Zach Bagley <szachbagley@gmail.com>
2025-10-03 09:18:59 -0600

    Initial Commit

---
+++ realspace/realspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  realspaceApp.swift
+//  realspace
+//
+//  Created by Zach Bagley on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct realspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
